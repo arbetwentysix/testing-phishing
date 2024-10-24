c3cfaa8a3de2324026fce389462891884f92be0c
arbetwentysix <lymonzx098@gmail.com>
2024-10-24 16:28:03 +0700

    update3

--- models/db.js
+++ models/db.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
   connectionString: 'postgres://default:uFeDR95xngaw@ep-twilight-cake-a4jlzhv1-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require',
